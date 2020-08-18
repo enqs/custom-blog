bda0247621ccca24d796a550d8b14ac7a9de5079
Andrzej <redmor13@gmail.com>
2020-08-18 15:28:54 +0200

    Initial commit

---
+++ src/main/java/enqs/boardgameblog/BoardGameBlogApplication.java
@@ -0,0 +1,13 @@
+package enqs.boardgameblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enqs/boardgameblog/BoardGameBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package enqs.boardgameblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
