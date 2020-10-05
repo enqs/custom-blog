9617063f3319e92ee2fa877846c66ce3025b9f29
Andrzej <redmor13@gmail.com>
2020-10-05 20:30:29 +0200

    Clean methods of UserServiceTest

--- src/test/java/enqs/customblog/service/UserServiceImplTest.java
+++ src/test/java/enqs/customblog/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package enqs.customblog.service;
 
 import enqs.customblog.dao.UserRepository;
+import enqs.customblog.entity.Article;
 import enqs.customblog.entity.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,6 +27,7 @@ class UserServiceImplTest {
     private User sampleUserFoo;
     private User sampleUserBar;
     private User sampleUserBaz;
+    private List<User> users;
 
     @BeforeEach
     void setUp() {
@@ -40,6 +42,99 @@ class UserServiceImplTest {
     }
 
     @Test
+    void shouldFindExactlyAllPersistedUsers() {
+        //GIVEN
+        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
+
+        //WHEN
+        List<User> users = userService.findAll();
+
+        //THEN
+        Assertions.assertThat(users).containsExactlyInAnyOrderElementsOf(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenNoUserPersisted() {
+        //GIVEN
+        //empty db ;)
+
+        //WHEN
+        List<User> users = userService.findAll();
+
+        //THEN
+        Assertions.assertThat(users).hasSize(0);
+    }
+
+
+    @Test
+    void shouldNotModifyNorDeleteUsersWhenFindingAll() {
+        //GIVEN
+        List<User> users = List.of(this.sampleUserFoo, sampleUserBar, sampleUserBaz);
+        userRepository.saveAll(users);
+
+        //WHEN
+        userService.findAll();
+
+        //THEN
+        Assertions.assertThat(userRepository.findAll()).containsExactlyInAnyOrderElementsOf(users);
+    }
+
+    @Test
+    void shouldFindExistingUserById() {
+        //GIVEN
+        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
+        int targetId = sampleUserBar.getId();
+
+        //WHEN
+        User user = userService.findById(targetId);
+
+        //THEN
+        Assertions.assertThat(user).isEqualToComparingFieldByField(sampleUserBar);
+    }
+
+    @Test
+    void shouldNotDeleteNorModifyUsersWhenFindingUserById() {
+        //GIVEN
+        users = List.of(sampleUserFoo, sampleUserBar, sampleUserBaz);
+        userRepository.saveAll(users);
+        int targetId = sampleUserBar.getId();
+
+        //WHEN
+        userService.findById(targetId);
+
+        //THEN
+        Assertions.assertThat(userRepository.findAll()).containsExactlyInAnyOrderElementsOf(users);
+    }
+
+    @Test
+    void shouldNotFindUserThatDoesNotExist() {
+        //GIVEN
+        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
+        int targetId = sampleUserBaz.getId() * 10;
+
+        //WHEN
+        User user = userService.findById(targetId);
+
+        //THEN
+        //ToDo: Should expect exception
+        Assertions.assertThat(user).isEqualToComparingFieldByField(new User());
+    }
+
+    @Test
+    void shouldNotDeleteNorModifyUsersWhenFailingToFindingUserById() {
+        //GIVEN
+        users = List.of(sampleUserFoo, sampleUserBar, sampleUserBaz);
+        userRepository.saveAll(users);
+        int targetId = sampleUserBar.getId() * 80;
+
+        //WHEN
+        userService.findById(targetId);
+
+        //THEN
+        Assertions.assertThat(userRepository.findAll()).containsExactlyInAnyOrderElementsOf(users);
+    }
+
+    @Test
     void shouldSaveUserOneTime() {
         //WHEN
         userService.save(sampleUserFoo);
@@ -50,7 +145,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void shouldEncryptNewPassword() {
+    void shouldEncryptNewUserPasswordWhenSavingUser() {
         //GIVEN
         String password = "samplePassword";
         sampleUserFoo.setPassword(password);
@@ -63,7 +158,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void shouldNotChangeExistingPasswordWhenNewPasswordIsNull() {
+    void shouldNotChangeExistingUserPasswordWhenNewPasswordIsNull() {
         //GIVEN
         String originalPassword = "samplePassword";
         sampleUserFoo.setPassword(passwordEncoder.encode(originalPassword));
@@ -92,7 +187,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void shouldNotChangeRoleWhenAlreadySpecified() {
+    void shouldNotChangeUserRoleWhenAlreadySpecified() {
         //GIVEN
         String originalRole = sampleUserFoo.getRole();
 
@@ -104,7 +199,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void shouldNotModifyNorDeleteOtherEntriesWhenSaveNew() {
+    void shouldNotModifyNorDeleteOtherUsersWhenSaveNew() {
         //GIVEN
         userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar));
         userRepository.flush();
@@ -118,7 +213,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void shouldNotModifyNorDeleteOtherEntriesWhenSaveEdited() {
+    void shouldNotModifyNorDeleteOtherUsersWhenSaveEdited() {
         //GIVEN
         userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar));
         userRepository.flush();
@@ -133,75 +228,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void shouldModifyOnlyTargetEntity() {
-        //GIVEN
-        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
-        userRepository.flush();
-
-        //WHEN
-        sampleUserFoo.setFirstName("NewFirstName");
-        sampleUserFoo.setLastName("NewLastName");
-        userService.save(sampleUserFoo);
-
-
-        //THEN
-        List<User> users = userRepository.findAll();
-        Assertions.assertThat(users).containsExactlyInAnyOrderElementsOf(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
-    }
-
-    @Test
-    void shouldFindExistingEntityById() {
-        //GIVEN
-        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
-        int targetId = sampleUserBar.getId();
-
-        //WHEN
-        User user = userService.findById(targetId);
-
-        //THEN
-        Assertions.assertThat(user).isEqualToComparingFieldByField(sampleUserBar);
-    }
-
-    @Test
-    void shouldNotFindEntityThatDoesNotExist() {
-        //GIVEN
-        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
-        int targetId = sampleUserBaz.getId() * 10;
-
-        //WHEN
-        User user = userService.findById(targetId);
-
-        //THEN
-        //ToDo: Should expect exception
-        Assertions.assertThat(user).isEqualToComparingFieldByField(new User());
-    }
-
-    @Test
-    void shouldFindExactlyAllPersistedEntities() {
-        //GIVEN
-        userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
-
-        //WHEN
-        List<User> users = userService.findAll();
-
-        //THEN
-        Assertions.assertThat(users).containsExactlyInAnyOrderElementsOf(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
-    }
-
-    @Test
-    void shouldReturnEmptyListWhenNothingPersisted() {
-        //GIVEN
-        //empty db ;)
-
-        //WHEN
-        List<User> users = userService.findAll();
-
-        //THEN
-        Assertions.assertThat(users).hasSize(0);
-    }
-
-    @Test
-    void shouldDeleteOnlyTargetEntity() {
+    void shouldDeleteOnlyTargetUser() {
         //GIVEN
         userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
         int targetId = sampleUserBaz.getId();
@@ -293,8 +320,8 @@ class UserServiceImplTest {
     void shouldThrowExceptionWhenUsernameDoesNotExist() {
         //GIVEN
         userRepository.saveAll(List.of(sampleUserFoo, sampleUserBar));
-        Assertions
 
+        Assertions
                 //WHEN
                 .assertThatThrownBy(() -> userService.findByUsername(sampleUserBaz.getUsername()))
 
@@ -351,11 +378,11 @@ class UserServiceImplTest {
         //GIVEN
         userRepository.save(sampleUserFoo);
 
-        Assertions.
+        Assertions
                 //WHEN
-                        assertThatThrownBy(() -> userService.loadUserByUsername(sampleUserBar.getUsername())).
+                        .assertThatThrownBy(() -> userService.loadUserByUsername(sampleUserBar.getUsername()))
                 //THEN
-                        isInstanceOf(UsernameNotFoundException.class);
+                        .isInstanceOf(UsernameNotFoundException.class);
     }
 
     @Test
