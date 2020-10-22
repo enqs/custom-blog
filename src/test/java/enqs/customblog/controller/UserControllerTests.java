2df83e1a363adc57b5672c6ff38e85c8126dbf39
Andrzej <redmor13@gmail.com>
2020-10-22 13:04:31 +0200

    Write tests for UserController.showUsers

--- src/test/java/enqs/customblog/controller/UserControllerTests.java
+++ src/test/java/enqs/customblog/controller/UserControllerTests.java
@@ -1,7 +1,9 @@
 package enqs.customblog.controller;
 
+import enqs.customblog.entity.User;
 import enqs.customblog.service.UserService;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,17 +23,60 @@ class UserControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private UserService userServiceMock;
 
-    @Test
-    void shouldReturnUsersTemplate() throws Exception {
-        Mockito.when(userServiceMock.findAll()).thenReturn(new ArrayList<>());
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/users"))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("users/users")));
+    private User sampleUserFoo;
+    private User sampleUserBar;
+    private User sampleUserBaz;
+
+    @BeforeEach
+    void setUp() {
+        sampleUserFoo = new User(1, "Username1", "Password1", "ROLE_USER", "Nick1", "FirstName1", "LastName1");
+        sampleUserBar = new User(2, "Username2", "Password2", "ROLE_USER", "Nick2", "FirstName2", "LastName2");
+        sampleUserBaz = new User(3, "Username3", "Password3", "ROLE_USER", "Nick3", "FirstName3", "LastName3");
+        Mockito.when(userServiceMock.findAll()).thenReturn(List.of(sampleUserFoo, sampleUserBar, sampleUserBaz));
+        Mockito.when(userServiceMock.findById(1)).thenReturn(sampleUserFoo);
+        Mockito.when(userServiceMock.findById(2)).thenReturn(sampleUserBar);
+        Mockito.when(userServiceMock.findById(3)).thenReturn(sampleUserBaz);
     }
+
+    @Test
+    void showUsersShouldReturnOkStatus() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void showUsersShouldReturnUsersTemplate() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.view().name("users/users"));
+    }
+
+    @Test
+    void showUsersShouldIncludeUsersAttributeToModel() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("users"));
+    }
+
+    @Test
+    void showUsersShouldIncludeUsersProvidedByServiceToModel() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.model()
+                        .attribute("users", Matchers.containsInAnyOrder(sampleUserFoo, sampleUserBar, sampleUserBaz)));
+    }
+
+    @Test
+    void showUsersShouldIncludeExactUserAmountProvidedByServiceToModel() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.model()
+                        .attribute("users", Matchers.hasSize(3)));
+    }
+
 
 }
